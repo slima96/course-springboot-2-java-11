15ac8c9e45beff87a6680977d07f47647853c343
Alexsandro Leão Lima <sandro.lima@viatek.local>
2020-08-16 02:41:55 -0300

    Exception handling - findById

--- src/main/java/com/viatekbrasil/course/services/UserService.java
+++ src/main/java/com/viatekbrasil/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.viatekbrasil.course.entities.User;
 import com.viatekbrasil.course.repositories.UserRepository;
+import com.viatekbrasil.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
